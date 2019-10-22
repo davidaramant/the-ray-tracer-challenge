b89189169a102f08527b3d5a9cff1c6eaa403ea4
David Aramant <davidaramant@gmail.com>
2019-10-21 21:57:19 -0400

    More code cleanup

--- RayTracer/RayTracer.WpfGui/MainWindow.xaml.cs
+++ RayTracer/RayTracer.WpfGui/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Numerics;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using RayTracer.Core;
@@ -14,9 +11,6 @@ namespace RayTracer.WpfGui
     {
         private readonly FastImage _canvas;
         private readonly WriteableBitmap _guiCanvas;
-        private readonly KeysPressed _input = new KeysPressed();
-        private readonly Stopwatch _timer = new Stopwatch();
-        private TimeSpan _lastElapsed;
 
         public MainWindow()
         {
@@ -45,11 +39,6 @@ namespace RayTracer.WpfGui
             _guiCanvas.Clear(Colors.Black);
 
             CompositionTarget.Rendering += GameLoop;
-            KeyDown += OnKeyDown;
-            KeyUp += OnKeyUp;
-
-            _timer.Start();
-            _lastElapsed = _timer.Elapsed;
 
             this.Loaded += MainWindow_Loaded;
         }
@@ -59,29 +48,8 @@ namespace RayTracer.WpfGui
             await Renderer.TraceScene(_canvas.Dimensions, _canvas.SetPixel);
         }
 
-        private void OnKeyDown(object sender, KeyEventArgs e)
-        {
-            // This method avoids any branching
-            _input.Up |= e.Key == Key.Up;
-            _input.Down |= e.Key == Key.Down;
-            _input.Left |= e.Key == Key.Left;
-            _input.Right |= e.Key == Key.Right;
-        }
-
-        private void OnKeyUp(object sender, KeyEventArgs e)
-        {
-            // This method avoids any branching
-            _input.Up &= e.Key != Key.Up;
-            _input.Down &= e.Key != Key.Down;
-            _input.Left &= e.Key != Key.Left;
-            _input.Right &= e.Key != Key.Right;
-        }
-
         void GameLoop(object sender, EventArgs e)
         {
-            var timeStamp = _timer.Elapsed;
-            _lastElapsed = timeStamp;
-
             _guiCanvas.WritePixels(new Int32Rect(0, 0, _canvas.Width, _canvas.Height), _canvas.GetBuffer(), _canvas.Stride, 0);
         }
     }
