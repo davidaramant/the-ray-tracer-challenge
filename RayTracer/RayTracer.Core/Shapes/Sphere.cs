e0bd6f6a059765e93fcb71dcb7d37cc84dcc8bfb
David Aramant <davidaramant@gmail.com>
2019-11-02 16:32:54 -0400

    More precise ray / sphere intersection method

--- RayTracer/RayTracer.Core/Shapes/Sphere.cs
+++ RayTracer/RayTracer.Core/Shapes/Sphere.cs
@@ -16,6 +16,41 @@ namespace RayTracer.Core.Shapes
 
         public override string ToString() => Name;
 
+        private bool SolveQuadratic(float a, float b, float c, ref float t0, ref float t1)
+        {
+            void Swap(ref float one, ref float two)
+            {
+                var temp = one;
+                one = two;
+                two = temp;
+            }
+
+            float discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
+            {
+                return false;
+            }
+            else if (discriminant.IsZero())
+            {
+                t0 = t1 = -0.5f * b / a;
+            }
+            else
+            {
+                float q = b > 0 ?
+                    -0.5f * (b + Sqrt(discriminant)) :
+                    -0.5f * (b - Sqrt(discriminant));
+                t0 = q / a;
+                t1 = c / q;
+            }
+
+            if (t0 > t1)
+            {
+                Swap(ref t0, ref t1);
+            }
+
+            return true;
+        }
+
         public override List<Intersection> LocalIntersect(Ray localRay)
         {
             var sphereToRay = localRay.Origin;
@@ -24,21 +59,18 @@ namespace RayTracer.Core.Shapes
             var a = Dot(localRay.Direction, localRay.Direction);
             var b = 2 * Dot(localRay.Direction, sphereToRay);
             var c = Dot(sphereToRay, sphereToRay) - 1;
-
-            var discriminant = b * b - 4 * a * c;
-
-            if (discriminant < 0)
+            float t0 = 0;
+            float t1 = 0;
+            if (!SolveQuadratic(a, b, c, ref t0, ref t1))
             {
                 return new List<Intersection>();
             }
-            else
+
+            return new List<Intersection>
             {
-                return new List<Intersection>
-                {
-                    new Intersection((-b - Sqrt(discriminant)) / (2*a), this),
-                    new Intersection((-b + Sqrt(discriminant)) / (2*a), this),
-                };
-            }
+                new Intersection(t0, this),
+                new Intersection(t1, this),
+            };
         }
 
         public override Vector4 GetLocalNormalAt(Vector4 localPoint)
