c35caa5cbc51d6476e0187ba4c9a4c7dc2bf2c59
David Aramant <davidaramant@gmail.com>
2019-11-04 16:59:11 -0500

    Camera movement! Forward/back and turn right/left

--- RayTracer/RayTracer.WpfGui/MainWindow.xaml.cs
+++ RayTracer/RayTracer.WpfGui/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,6 +20,13 @@ namespace RayTracer.WpfGui
         private readonly Camera _camera;
         private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private Task _renderingTask;
+        private readonly Stopwatch _timer = new Stopwatch();
+        private TimeSpan _lastElapsed;
+
+        // world units per ms
+        private const float MovementSpeed = 0.0001f;
+        private const float RotationSpeed = 0.00005f;
+
 
         public MainWindow()
         {
@@ -50,7 +59,11 @@ namespace RayTracer.WpfGui
             CompositionTarget.Rendering += GameLoop;
             KeyDown += MainWindow_KeyDown;
 
-            this.Loaded += (s,e) => StartRenderingScene();
+            this.Loaded += (s, e) =>
+            {
+                _timer.Start();
+                StartRenderingScene();
+            };
         }
 
         private async void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -91,11 +104,53 @@ namespace RayTracer.WpfGui
             await Renderer.TraceScene(_camera, _world, _canvas.SetPixel, _cancellationTokenSource.Token);
         }
 
-        void GameLoop(object sender, EventArgs e)
+        async void GameLoop(object sender, EventArgs e)
         {
-            _guiCanvas.WritePixels(new Int32Rect(0, 0, _canvas.ActualDimensions.Width, _canvas.ActualDimensions.Height), 
-                _canvas.GetBuffer(), 
+            var timeStamp = _timer.Elapsed;
+            await UpdateCameraPosition(timeStamp - _lastElapsed);
+            _lastElapsed = timeStamp;
+
+            _guiCanvas.WritePixels(new Int32Rect(0, 0, _canvas.ActualDimensions.Width, _canvas.ActualDimensions.Height),
+                _canvas.GetBuffer(),
                 _canvas.Stride, 0);
+        }
+
+        async Task UpdateCameraPosition(TimeSpan frameTime)
+        {
+            float inlineDistance = MovementSpeed * (float)frameTime.TotalMilliseconds;
+            float rotationAmount = RotationSpeed * (float) frameTime.TotalMilliseconds;
+            bool moved = false;
+
+            Matrix4x4 movementMatrix = Matrix4x4.Identity;
+
+            if (Keyboard.IsKeyDown(Key.Up))
+            {
+                moved = true;
+                movementMatrix *= Matrix4x4.CreateTranslation(0, 0, inlineDistance);
+            }
+            else if (Keyboard.IsKeyDown(Key.Down))
+            {
+                moved = true;
+                movementMatrix *= Matrix4x4.CreateTranslation(0, 0, -inlineDistance);
+            }
+
+            if (Keyboard.IsKeyDown(Key.Right))
+            {
+                moved = true;
+                movementMatrix *= Matrix4x4.CreateRotationY(-rotationAmount);
+            }
+            else if (Keyboard.IsKeyDown(Key.Left))
+            {
+                moved = true;
+                movementMatrix *= Matrix4x4.CreateRotationY(rotationAmount);
+            }
+
+            if (moved)
+            {
+                await CancelRendering();
+                _camera.Transform *= movementMatrix;
+                StartRenderingScene();
+            }
         }
     }
 }
