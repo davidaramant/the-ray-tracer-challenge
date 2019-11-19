3aa8b17fcffb38f5b39c2cc099450d901a15df04
David Aramant <davidaramant@gmail.com>
2019-11-19 09:42:58 -0500

    Added empty MonoGame GUI project

---
+++ RayTracer/RayTracer.MonoGameGui/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RayTracer.MonoGameGui
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
